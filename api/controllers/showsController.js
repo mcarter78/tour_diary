c23a2434072b9fb7aeedb64ca6f41fcff0b35442
mcarter78 <sjsharksftw@gmail.com>
2016-03-26 15:21:37 -0700

    adds shows controller

---
+++ api/controllers/showsController.js
@@ -0,0 +1,43 @@
+var Show = require('../models/show');
+
+var showsController = {
+  index: function(req, res) {
+    Show.find({}, function(err, shows) {
+      if (err) console.log(err);
+      res.json(shows);
+    });
+  },
+  show: function(req, res) {
+    var id = req.params.id;
+    Show.findById(id, function(err, show) {
+      if (err) console.log(err);
+      res.json(show);
+    });
+  },
+  create: function(req, res) {
+    var newShow = req.body;
+    Show.create(newShow, function(err, show) {
+      console.log('show created:', show);
+    });
+  },
+  update: function(req, res) {
+    var id = req.params.id;
+    var updatedShow = req.body;
+    Show.findById(id, function(err, show) {
+      if (err) console.log(err);
+      show.save(updatedShow, function(err, updShow) {
+        if (err) console.log(err);
+        console.log('show updated', updShow);
+      });
+    });
+  },
+  delete: function(req, res) {
+    var id = req.params.id;
+    Show.findByIdAndRemove(id, function(err){
+      if (err) console.log(err);
+      console.log('show deleted');
+    });
+  }
+};
+
+module.exports = showsController;
