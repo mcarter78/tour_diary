8c5ff6bca6a07b2f9b076cedf897c559614985f6
mcarter78 <sjsharksftw@gmail.com>
2016-03-26 15:08:55 -0700

    adds blog controller

---
+++ api/controllers/blogController.js
@@ -0,0 +1,36 @@
+var Blog = require('../models/blog');
+
+var blogController = {
+  index: function(req, res) {
+    Blog.find({}, function(err, posts) {
+      if (err) console.log(err);
+      res.json(posts);
+    });
+  },
+  create: function(req, res) {
+    var newPost = req.body;
+    Blog.create(newPost, function(err, post) {
+      console.log('post created:', post);
+    });
+  },
+  update: function(req, res) {
+    var id = req.params.id;
+    var updatedPost = req.body;
+    Blog.findById(id, function(err, post) {
+      if (err) console.log(err);
+      post.save(updatedPost, function(err, updPost) {
+        if (err) console.log(err);
+        console.log('post updated', updPost);
+      });
+    });
+  },
+  delete: function(req, res) {
+    var id = req.params.id;
+    Blog.findByIdAndRemove(id, function(err){
+      if (err) console.log(err);
+      console.log('post deleted');
+    });
+  }
+};
+
+module.exports = blogController;
