28bd30fada1dfc4b71aeaef3ccd47ff15560362a
mcarter78 <sjsharksftw@gmail.com>
2016-03-26 14:15:12 -0700

    builds express server

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost/tour_diary');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/views'));
+
+app.listen(port, function(err){
+  if (err) console.log(err);
+  console.log('listening on port:', port);
+});
